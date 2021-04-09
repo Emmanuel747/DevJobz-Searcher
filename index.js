8551d3d2d68cf9d885c14a635b2e3d0a7af3a0f3
Emmanuel Ejim <emmanueleejim@gmail.com>
2021-04-08 20:04:41 -0500

    Ok testing with VSCode

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("I'm done, time to to job search")
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+   console.log(`App 🦵🏿 on 4`)
+});
+
+app.get('/', (req, res) => {
+   console.log('My Request Object', req)
+   console.log('My Response Object', res)
+   res.send("Hello World, again")
+})
+
+console.log("I'm almost done, time to job search")
+
