50e2611d78add59cf1548fd1edc3b5c8d1c1d982
Emmanuel Ejim <emmanueleejim@gmail.com>
2021-04-09 15:56:10 -0500

    2/3rd completed of module-3

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+require('dotenv').config(); // this will read the .env file, if it exists
+
+const { PORT = 3000, WEATHER_KEY } = process.env;
+
 const express = require("express");
 const server = express();
+const axios = require('axios');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cowsay = require('cowsay');
+const Quote = require('inspirational-quotes');
+
+server.listen(PORT, () => {
+  // stuff
+});
+
+// server.listen(3000, () => {
+//    console.log('Server is up...');
+//  });
 
 server.use(express.static('public'));
 
 server.get('/hello', (req, res, next) => {
   res.send(`
   <html>
-  <head></head>
-  <body>
-    <h3>Hello!</h3>
-  </body>
+   <head> </head>
+   <body>
+      <h3>Hello!</h3>
+   </body>
   </html>
   `)
 });
@@ -23,23 +40,50 @@ server.get('/', (req, res) => {
 // sad static server
 server.use(express.static('public'));
 
-const morgan = require('morgan');
 server.use(morgan('dev'));
 
-server.use(express.static('public'));
-
-const bodyParser = require('body-parser');
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 
-server.post('/job-search', (req, res) => {
-   res.send({
-     searchData: req.body,
-     status: "PENDING",
-   })
+server.get('/cowspiration', (req, res) => {
+   const {text, author} = Quote.getQuote()
+   const cow = cowsay.say({
+     text: `${text}\n\n- ${author}`,
+     W: 80,
+   });
+   res.send({cow})
  });
 
-server.listen(3000, () => {
-  console.log('Server is up...');
-});
+ server.post('/job-search', async (req, res) => {
+   try {
+     const { description, fulltime } = req.body;
+ 
+     const URL = `https://jobs.github.com/positions.json?${
+       description ? `description=${ description }&` : ''
+     }${
+       fulltime ? 'fulltime=true' : ''
+     }`;
+ 
+     const { data } = await axios.get(URL);
+ 
+     res.send({ results: data });
+   } catch (error) {
+     res.send({ error });
+   }
+ });
 
+ server.get('/weather', async (req, res) => {
+   // build URL from key AND query strings
+   // make axios request
+   // send back data, or send back error
+   try {
+      const { lat, lon } = req.query;
+      const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${ lat }&lon=${ lon }&appid=${ WEATHER_KEY }`;
+  
+      const { data } = await axios.get(URL);
+      res.send({ results: data });
+    } catch (error) {
+      res.send({ error });
+    }
+  });
+  
