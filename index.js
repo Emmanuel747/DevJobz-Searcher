798522e9fa51ca71a77f0a63d9b23b25264b8bb6
Emmanuel Ejim <emmanueleejim@gmail.com>
2021-04-09 14:45:20 -0500

    Completed Module 1

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 const express = require("express");
-const app = express();
+const server = express();
 
-app.listen(3000, () => {
-   console.log(`App 🦵🏿 on 4`)
-});
+server.use(express.static('public'));
 
 server.get('/hello', (req, res, next) => {
-   res.send(`
-   <html>
-   <head></head>
-   <body>
-     <h3>Hello!</h3>
-   </body>
-   </html>
-   `)
+  res.send(`
+  <html>
+  <head></head>
+  <body>
+    <h3>Hello!</h3>
+  </body>
+  </html>
+  `)
+});
+
+server.get('/', (req, res) => {
+  // it's ok to omit next if you're not going to call it
+
+  res.send({ message: 'boop' })
+});
+
+// sad static server
+server.use(express.static('public'));
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.post('/job-search', (req, res) => {
+   res.send({
+     searchData: req.body,
+     status: "PENDING",
+   })
  });
- 
- server.listen(3000, () => {
-   console.log('I am listening...');
- });
+
+server.listen(3000, () => {
+  console.log('Server is up...');
+});
+
